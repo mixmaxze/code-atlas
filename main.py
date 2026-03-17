f7456c0a62a520f02ad31425b4d18ca42dbea32a
José Gama <jose.gama@ccc.ufcg.edu.br>
2026-03-17 11:50:47 -0300

    update vector index path logic

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from rag.chunker import chunk_files
 from embeddings.embedding_index import CodeEmbeddingIndex
 from rag.code_rag import CodeRAG
 
-repo_url = "https://github.com/i-voted-for-trump/is-even"
+repo_url = "https://github.com/keycloak/keycloak"
 
-index_path = "vector_index.pkl"
+index_dir = "vector_indexes"
+os.makedirs(index_dir, exist_ok=True)
+index_path = os.path.join(
+    index_dir,
+    f"{repo_url.split('.com/')[-1].replace('/', '-')}.pkl"
+)
 
 index = CodeEmbeddingIndex()
-
 # se índice já existir → carregar
 if os.path.exists(index_path):
 
@@ -36,7 +40,7 @@ else:
 
 rag = CodeRAG(index)
 
-question = "What is the main purpose of this library?"
+question = "Como eu executo a principal funcionalidade deste código?"
 
 answer = rag.ask(question)
 
